058c6e8807c0002f87825cb8df125f8d7079e5ec
Rai Adnan <officialshaheendevelopers@gmail.com>
2023-07-10 11:00:42 +0500

    Initial commit

---
+++ demo/src/main/java/com/raiadnan/ads/sdk/callback/CallbackConfig.java
@@ -0,0 +1,12 @@
+package com.raiadnan.ads.sdk.callback;
+
+import com.raiadnan.ads.sdk.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CallbackConfig {
+
+    public List<Post> android = new ArrayList<>();
+
+}
---
+++ demo/src/main/java/com/raiadnan/ads/sdk/model/Post.java
@@ -0,0 +1,10 @@
+package com.raiadnan.ads.sdk.model;
+
+import java.io.Serializable;
+
+public class Post implements Serializable {
+
+    public String name;
+    public String image;
+
+}
